c4f2e03b5bfb8638cbb8169970b0b82c5ed1a381
Yudas <yudasmalabi@gmail.com>
2021-12-22 17:19:08 +0700

    fix layout not compact

--- views/public/dashboard/order/list.php
+++ views/public/dashboard/order/list.php
@@ -10,7 +10,7 @@ $order = new OrderController();
 <div class="content-body">
     <!-- row -->
     <div class="container-fluid">
-        <div class="row order-row" id="masonry" style="position: relative; height: 859.2px;">
+        <div class="row" id="masonry">
             <?php foreach ($order->getAll() as $list) :
                 $status = $list->status_paid;
                 if ($status == 'PENDING') {
@@ -23,29 +23,33 @@ $order = new OrderController();
                     $color = 'bg-secondary';
                 }
                 $items = $order->getOrderDetails($list->invoice_id); ?>
-                <a href="<?= $uriHelper->baseUrl("index.php?page=dashboard&content=order&menu=detail&invoice_id=" . $list->invoice_id) ?>">
-                    <div class="card-container" style="position: absolute; left: 0px; top: 0px;">
-                        <div class="card shadow-sm">
-                            <div class="card-header <?= $color ?> text-white">
-                                <div>
-                                    <h4 class="text-white"><?= formHelper::rupiah($list->total_amount) ?></h4>
-                                    <span class="fs-12 op9">Invoice ID : <?= $list->invoice_id ?></span>
-                                    <br>
-                                    <span class="fs-12 op9"><?= $list->created_at ?></span>
+                <div class="col-xl-4 col-lg-6 col-sm-6">
+                    <a href="<?= $uriHelper->baseUrl("index.php?page=dashboard&content=order&menu=detail&invoice_id=" . $list->invoice_id) ?>">
+                        <div class="card-container">
+                            <div class="card shadow-sm">
+                                <div class="card-header <?= $color ?> text-white">
+                                    <div>
+                                        <h4 class="text-white"><?= formHelper::rupiah($list->total_amount) ?></h4>
+                                        <span class="fs-12 op9">Invoice ID : <?= $list->invoice_id ?></span>
+                                        <br>
+                                        <span class="fs-12 op9"><?= $list->created_at ?></span>
 
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <ul class="order-list">
+                                        <li>Pemesan: <span><?= $list->name ?></span></li>
+                                        <?php foreach ($items as $item) : ?>
+                                            <li><span><?= $item->qty ?></span><?= $item->name ?></li>
+                                        <?php endforeach; ?>
+                                    </ul>
                                 </div>
                             </div>
-                            <div class="card-body">
-                                <ul class="order-list">
-                                    <li>Pemesan: <span><?= $list->name ?></span></li>
-                                    <?php foreach ($items as $item) : ?>
-                                        <li><span><?= $item->qty ?></span><?= $item->name ?></li>
-                                    <?php endforeach; ?>
-                                </ul>
-                            </div>
                         </div>
-                    </div>
-                </a>
+                    </a>
+                </div>
+
+
             <?php endforeach; ?>
         </div>
     </div>
